ad2dd8fa4a4e710b62ead8b0d8e44f8c481c4bb1
BertrandConxy <90222110+BertrandConxy@users.noreply.github.com>
2021-12-21 10:57:03 +0200

    Correct linter errors

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-toggleOn = document.getElementById('menu-icons1');
-toggleOff = document.getElementById('menu-icon2');
-menuList = document.getElementById('menu-list');
+const toggleOn = document.getElementById('menu-icons1');
+const toggleOff = document.getElementById('menu-icon2');
+const menuList = document.getElementById('menu-list');
 
 toggleOn.addEventListener('click', () => {
-    menuList.classList.toggle('show');
-    toggleOff.classList.toggle('show');
-    toggleOn.classList.toggle('hide');
+  menuList.classList.toggle('show');
+  toggleOff.classList.toggle('show');
+  toggleOn.classList.toggle('hide');
 });
 
 toggleOff.addEventListener('click', () => {
-    menuList.classList.toggle('show');
-    toggleOff.classList.toggle('show');
-    toggleOn.classList.toggle('hide');
+  menuList.classList.toggle('show');
+  toggleOff.classList.toggle('show');
+  toggleOn.classList.toggle('hide');
 });
 
-document.onclick =  function(e){
-    if(e.target.id !== 'menu-list' && e.target.id !== 'menu-icons1' && e.target.id !== 'menu-icon2'){
-        menuList.classList.remove('show');
-        toggleOn.classList.remove('hide');
-        toggleOff.classList.remove('show');
-    }
-}
+document.onclick = function remove(e) {
+  if (e.target.id !== 'menu-list' && e.target.id !== 'menu-icons1' && e.target.id !== 'menu-icon2') {
+    menuList.classList.remove('show');
+    toggleOn.classList.remove('hide');
+    toggleOff.classList.remove('show');
+  }
+};
